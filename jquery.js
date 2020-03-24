a0f862dd652185672dd0f2620429ebe5d148e245
Alvin Galit <aroblesgalit@gmail.com>
2020-03-24 17:13:42 -0500

    Added logic for storing array and getting it and rendering it to the DOM

--- jquery.js
+++ jquery.js
@@ -3,36 +3,74 @@ $(document).ready(function () {
     // Target elements
     var todoForm = $("#todo-form");
     var userInput = $("#todoInput");
+    var todoList = $("#todo-list");
 
     // Declare variable for todo list array and set it to an empty array
     var todosArray = [];
 
+    init();
+
     // Add event listener to submit button
-    todoForm.on("click", function (e) {
+    todoForm.on("submit", function (e) {
         // prevent default
         e.preventDefault();
 
-        element = e.target;
-        // When submit button is clicked
-        if (element.value === "Submit") {
+        var todoText = userInput.val().trim();
 
-            if (userInput.val() !== "") {
+        if (userInput.val() !== "") {
             // take value of input and store it inside array
-            todosArray.push(userInput.val());
-            console.log(todosArray);
-            }
-
-
+            todosArray.push(todoText);
+            // clear the input
+            userInput.val("");
         }
+
+        // Store array in localStorage
+        storeArray();
+
+        // Render UI
+        renderList();
+
     })
 
+    function storeArray() {
+        // Stringify and set todosArray key in localStorage to the todosArray
+        localStorage.setItem("todosArray", JSON.stringify(todosArray));
+    }
 
-    // Store array in localStorage
-    // Get array from localStorage
-    // Create li element for each item in array
 
-    // Render ui
-    // Delete what's in the input field
+    // Render ui function
+    function renderList() {
+        // Clear ul
+        todoList.html("");
+
+        // Loop through storage array
+        for (var i = 0; i < todosArray.length; i++) {
+            // Create li element for each item in array
+            var todoItemLi = $("<li>").addClass("todo-item").attr("data-index", i);
+            // Create delete button
+            var deleteButton = $("<i>").addClass("fa fa-times closeButton");
+            // Set text to current value and append the button
+            todoItemLi.text(todosArray[i]).append(deleteButton);
+            // Append to ul
+            todoList.append(todoItemLi);
+        }
+
+    }
+
+    function init() {
+        // Get array from localStorage
+        // Parse JSON string to object
+        var storageArray = JSON.parse(localStorage.getItem("todosArray"));
+
+        // If array were retrieved from localStorage, update the todos array to it
+        if (storageArray !== null) {
+            todosArray = storageArray;
+        }
+
+        // Render array to the DOM
+        renderList();
+    }
+
 
 
     // When x button is clicked
